0205686ab9fcfbbe855b8089e3da4c144fd2a36e
GraceNap <6540100622@student.chula.ac.th>
2023-12-11 21:06:44 +0700

    Update summar.py

--- summar.py
+++ summar.py
@@ -4,11 +4,10 @@ import json
 import pandas as pd
 import nltk
 from collections import Counter
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 from newspaper import Article
-from nltk.corpus import wordnet
-from translate import Translator  # Use translate library for translation
+from translate import Translator
 
 # Download the stopwords resource
 nltk.download('stopwords')
@@ -29,9 +28,26 @@ def send_prompt(prompt, model="gpt-3.5-turbo"):
     )
     return response.choices[0].message.content
 
+def process_input(input_text, model="gpt-3.5-turbo", max_chunk_length=1000):
+    chunks = [input_text[i:i + max_chunk_length] for i in range(0, len(input_text), max_chunk_length)]
+    processed_chunks = []
+
+    for chunk in chunks:
+        messages_so_far = [
+            {"role": "system", "content": "Obtain URL. Text Preprocessing. Summarizing the Article"},
+            {'role': 'user', 'content': chunk},
+        ]
+        response = client.chat.completions.create(
+            model=model,
+            messages=messages_so_far
+        )
+        processed_chunks.append(response.choices[0].message.content)
+
+    return " ".join(processed_chunks)
+
 st.title('Summarization genius')
-st.markdown('Input the article URL that you want to summarize.\n'
-            'The AI will provide a summary and vocabulary commonly found in TOEIC Exam with meanings and synonyms.')
+st.markdown('Input the English article URL that you want to summarize.\n'
+            'The AI will provide a summary in English and in Spanish, also vocabulary commonly found in TOEIC C1 level with meanings and synonyms.')
 
 url_input = st.text_input("Enter the article URL:", "Your URL here")
 
@@ -46,22 +62,31 @@ if st.button('Submit'):
         # Text Preprocessing
         user_input = url.text
 
-        # Dynamically adjust the length of the input
-        max_input_length = 450  # Set the maximum length within the API limit
-        total_length = 0
-        summary = ""
+        # Summarize the article using GPT-3.5-turbo
+        messages_so_far = [
+            {"role": "system", "content": "Obtain URL. Text Preprocessing. Summarizing the Article"},
+            {'role': 'user', 'content': user_input},
+        ]
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages_so_far
+        )
 
-        for sentence in nltk.sent_tokenize(user_input):
-            total_length += len(sentence)
-            if total_length <= max_input_length:
-                prompt = summary + " " + sentence if total_length > len(sentence) else sentence
-                chunk_summary = send_prompt(prompt)
-                summary += chunk_summary
-            else:
-                break
-
-        st.markdown('**AI response:**')
+        st.markdown('**English summary:**')
+        summary = response.choices[0].message.content
         st.write(summary)
+
+        translator = Translator(to_lang='es')
+        summary_spanish = translator.translate(summary)
+        st.markdown('**Summary in Spanish:**')
+        st.write(summary_spanish)
+
+
+
+        
+
+
+    
 
         # Tokenize the article to find important words
         tokens = nltk.word_tokenize(user_input.lower())
@@ -75,28 +100,30 @@ if st.button('Submit'):
         toefl_words = list(set([word for word, freq in word_freq.most_common() if freq > 1]))  # Remove duplicates
 
         # Translate the summary to Spanish using the translate library
-        translator = Translator(to_lang='es')
-        summary_spanish = translator.translate(summary)
-        st.markdown('**Summary in Spanish:**')
-        st.write(summary_spanish)
+        # translator = Translator(to_lang='es')
+        # summary_spanish = translator.translate(summary)
+        # st.markdown('**Summary in Spanish:**')
+        # st.write(summary_spanish)
 
         # Create a DataFrame to display words, meanings, and synonyms
         vocab_data = {'Words': [], 'Meanings': [], 'Synonyms': []}
 
         for word in toefl_words:
             # Get meanings and synonyms for each word
-            synsets = wordnet.synsets(word)
+            synsets = nltk.corpus.wordnet.synsets(word)
 
             if synsets:
                 meanings = synsets[0].definition()
                 synonyms = [lemma.name() for syn in synsets for lemma in syn.lemmas()]
                 vocab_data['Words'].append(word)
                 vocab_data['Meanings'].append(meanings)
-                vocab_data['Synonyms'].append(', '.join(synonyms))
+                if synonyms in vocab_data['Synonyms']:
+                    pass
+                elif synonyms not in vocab_data['Synonyms']:
+                     vocab_data['Synonyms'].append(', '.join(synonyms))
 
         vocab_df = pd.DataFrame(vocab_data)
         st.table(vocab_df)
 
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
-
